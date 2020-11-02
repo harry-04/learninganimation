c21d6c3506f4058887ab8e8ad5c024b27d9c2e47
harry-04 <72386526+harry-04@users.noreply.github.com>
2020-11-02 16:18:28 +0000

    yes

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animation : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    private Animator anim;
+    public float moveSpeed = 8f;
+
+
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKey("a"))
+        {
+            rb.velocity = new Vector2(-moveSpeed, rb.velocity.y);
+            anim.SetBool("walk_left", true);
+        }
+        else
+        {
+            anim.SetBool("walk_left", false);
+        }
+
+        if (Input.GetKey("d"))
+        {
+            rb.velocity = new Vector2(+moveSpeed, rb.velocity.y);
+            anim.SetBool("walk_right", true);
+        }
+        else
+        {
+            anim.SetBool("walk_right", false);
+        }
+
+    }
+}
